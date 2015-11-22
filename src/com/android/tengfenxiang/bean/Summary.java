5fc326bf4c3aebae46e73d1f5ca110b81fd63a80
sixtyman <sixtyman@github.com>
2015-11-22 23:28:47 +0800

    调整bean类，增加开源网络库Volley

--- src/com/android/tengfenxiang/bean/Summary.java
+++ src/com/android/tengfenxiang/bean/Summary.java
@@ -69,4 +69,38 @@ public class Summary {
 		this.recent = recent;
 	}
 
+	/**
+	 * 内部类表示曲线上的某个点
+	 * @author ccz
+	 *
+	 */
+	public static class ProfitPoint {
+
+		/**
+		 * 横坐标，日期
+		 */
+		private String profitDate;
+
+		/**
+		 * 纵坐标，收益值
+		 */
+		private float points;
+
+		public String getProfitDate() {
+			return profitDate;
+		}
+
+		public void setProfitDate(String profitDate) {
+			this.profitDate = profitDate;
+		}
+
+		public float getPoints() {
+			return points;
+		}
+
+		public void setPoints(float points) {
+			this.points = points;
+		}
+
+	}
 }

9d7addc28194177072a63848db0172021855a984
sixtyman <sixtyman@github.com>
2015-11-22 23:32:33 +0800

    调整bean类，增加开源网络库Volley

---
+++ src/com/android/tengfenxiang/util/Constant.java
@@ -0,0 +1,14 @@
+package com.android.tengfenxiang.util;
+
+/**
+ * 定义系统中的常量
+ * @author ccz
+ *
+ */
+public class Constant {
+
+	/**
+	 * 服务器的IP地址
+	 */
+	public static String serverAddress = "";
+}

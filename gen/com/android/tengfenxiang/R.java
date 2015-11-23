876be43a96c802e570b1969e73553a75abc677df
sixtyman <sixtyman@github.com>
2015-11-23 10:47:10 +0800

    增加XListView控件

--- gen/com/android/tengfenxiang/R.java
+++ gen/com/android/tengfenxiang/R.java
@@ -10,6 +10,9 @@ package com.android.tengfenxiang;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,26 +21,44 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int xlistview_arrow=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090009;
+        public static final int xlistview_footer_content=0x7f090000;
+        public static final int xlistview_footer_hint_textview=0x7f090002;
+        public static final int xlistview_footer_progressbar=0x7f090001;
+        public static final int xlistview_header_arrow=0x7f090007;
+        public static final int xlistview_header_content=0x7f090003;
+        public static final int xlistview_header_hint_textview=0x7f090005;
+        public static final int xlistview_header_progressbar=0x7f090008;
+        public static final int xlistview_header_text=0x7f090004;
+        public static final int xlistview_header_time=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int xlistview_footer=0x7f030001;
+        public static final int xlistview_header=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int clear_cache_success=0x7f050003;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int clear_cache_success=0x7f060003;
+        public static final int hello_world=0x7f060001;
+        public static final int xlistview_footer_hint_normal=0x7f060008;
+        public static final int xlistview_footer_hint_ready=0x7f060009;
+        public static final int xlistview_header_hint_loading=0x7f060006;
+        public static final int xlistview_header_hint_normal=0x7f060004;
+        public static final int xlistview_header_hint_ready=0x7f060005;
+        public static final int xlistview_header_last_time=0x7f060007;
     }
     public static final class style {
         /** 
@@ -61,10 +82,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
